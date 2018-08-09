623025abfef9f4313402a0e1b936dc8c2db81127
mohitkirange <mohitk@csu.fullerton.edu>
2018-08-09 16:40:32 -0700

    Sticky Smooth Active Navigation

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let NavLinks = document.querySelectorAll("nav ul li a");
+let Sections = document.querySelectorAll("main section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  NavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
